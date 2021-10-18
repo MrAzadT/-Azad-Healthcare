7369d25531bf80ba410a019a2bd635e10f41e8bb
azadtamal <mrazadt@gmail.com>
2021-10-18 12:22:35 +0600

    function

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import "./App.css";
-
+import Navbar from "./components/Navbar/Navbar";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Home from "./components/Home/Home";
 function App() {
   return (
-    <div className="App">
-      <h1>azad</h1>
-    </div>
+    <Router>
+      <Switch>
+        <Navbar />
+        <Route path="/home">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
