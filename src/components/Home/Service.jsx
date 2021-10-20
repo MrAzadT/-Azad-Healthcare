f6a0284d33988511dd26aac90af8e394a2c4b752
azadtamal <mrazadt@gmail.com>
2021-10-20 12:16:00 +0600

    done

--- src/components/Home/Service.jsx
+++ src/components/Home/Service.jsx
@@ -9,10 +9,11 @@ const Service = () => {
   const item = myData.find((item) => item.id === Number(serviceDetails));
 
   return (
-    <div className="mt-8">
+    <div className="mt-8 border-1 border-gray-300 bg-gray-200 shadow-lg  h-3/5 m-auto rounded-lg mb-10 w-3/5 p-10 ">
+      <h1 className=" my-10 text-4xl">Service Details :</h1>
       <img className="rounded-t-lg w-96 h-80" src={item.img} alt="" />
 
-      <div className="p-5">
+      <div className="p-5 ">
         <h5 className="text-gray-900 font-bold text-2xl tracking-tight mb-2">
           {item.name}
         </h5>
@@ -20,9 +21,9 @@ const Service = () => {
         <p className="font-normal text-gray-700 mb-3">1. {item.h1}</p>
         <p className="font-normal text-gray-700 mb-3">2. {item.h2}</p>
         <p className="font-normal text-gray-700 mb-3">3. {item.p1}</p>
-        <p className="font-normal text-gray-700 mb-3">3. {item.p2}</p>
-        <p className="font-normal text-gray-700 mb-3">3. {item.p3}</p>
-        <p className="font-normal text-gray-700 mb-3">3. {item.p4}</p>
+        <p className="font-normal text-gray-700 mb-3">4. {item.p2}</p>
+        <p className="font-normal text-gray-700 mb-3">5. {item.p3}</p>
+        <p className="font-normal text-gray-700 mb-3">6. {item.p4}</p>
       </div>
     </div>
   );
